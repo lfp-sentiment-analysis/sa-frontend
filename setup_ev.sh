e389ff1a66ddce486550c288724d3938fe9933cc
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-10-28 17:40:53 +0800

    Updated Travis

--- setup_ev.sh
+++
@@ -1,4 +0,0 @@
-#export GOOGLE_APPLICATION_CREDENTIALS='./cits3200.json'
-export GOOGLE_APPLICATION_CREDENTIALS='/Users/yiran/desktop/finalcits3200/cits3200-f-frontend/cits3200.json'
-# Windows: type in command line: set GOOGLE_APPLICATION_CREDENTIALS=cits3200.json
-
